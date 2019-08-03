510ef6bcc989c902aae514eab09f836b419075d8
Nanda Oktavera <codeharuka.yusa@gmail.com>
2019-08-03 14:27:14 +0900

    Introduce zipfile builder
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

---
+++ buildzip.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Simple script for builing zip file
+#
+
+# get kernel.conf for id and version information
+source ./kernel.conf
+
+# Setup file zip name
+zipname=bootwitch-${kernelid// }-$kernelver.zip
+
+# Setup folder and files that will be included in the zip
+sources=(META-INF
+         external
+         kernel.conf
+         dtbo.img
+         Image.gz-dtb)
+
+# prepare working directory in the /tmp
+WORKDIR=/tmp/build-bootwitch-$USER
+rm -rf $WORKDIR
+mkdir $WORKDIR
+
+# copy needed files
+for file in "${sources[@]}"; do
+    (test -f $file || test -d $file) && cp -afv $file $WORKDIR/
+done
+
+# creating zip file
+command pushd "$WORKDIR" > /dev/null
+    zip -r9 --exclude=*placeholder $WORKDIR/$zipname *
+command popd > /dev/null
+
+# copy generated zip file to current directory
+cp -v $WORKDIR/$zipname $(pwd)/
+
+# cleanup working directory
+rm -rf $WORKDIR
