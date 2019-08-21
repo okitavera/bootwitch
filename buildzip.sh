abc4535f33a21867514854b333bb0d4852a89ccb
Nanda Oktavera <codeharuka.yusa@gmail.com>
2019-08-21 09:46:01 +0900

    buildzip: get needed files and info from KBUILD_OUT
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- buildzip.sh
+++ buildzip.sh
@@ -3,9 +3,20 @@
 # Simple script for builing zip file
 #
 
+# KBUILD_OUT, used to get kernel, dtbo file, and version directly from it
+[[ -f "$KBUILD_OUT/.config" ]] || { echo "missing $KBUILD_OUT. exiting"; exit 1; }
+
+# replace kernel version with actual version
+generatedver=$(cat $KBUILD_OUT/include/generated/utsrelease.h | cut -d\" -f2 | cut -d\- -f3)
+sed -i -e "s/kernelver=.*/kernelver=$generatedver/g" ./kernel.conf
+
 [[ -f "./kernel.conf" ]] || { echo "./kernel.conf cannot be found. exiting"; exit 1; }
 # get kernel.conf for id and version information
 source ./kernel.conf
+
+# replace kernel and dtbo path directly from KBUILD_OUT
+src_kernel=$KBUILD_OUT/arch/arm64/boot/$src_kernel
+src_dtbo=$KBUILD_OUT/$src_dtbo
 
 # Requirement checking
 [[ -f "./external/magiskboot" ]] || { echo "./external/magiskboot cannot be found. exiting"; exit 1; }
@@ -14,7 +25,7 @@ source ./kernel.conf
 [[ "$kernelver" ]] || { echo "kernelver cannot be found. exiting"; exit 1; }
 
 # Setup file zip name
-zipname=bootwitch-${kernelid// }-$kernelver.zip
+zipname=${kernelid// }-$kernelver-Mi9SE.zip
 
 # Setup folder and files that will be included in the zip
 sources=(META-INF
